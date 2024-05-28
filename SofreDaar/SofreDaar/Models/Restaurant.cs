0de3727e05d0a9896b1bddab25b02d28e07230c8
sseeyyeedd <sseeyyeeddaalliisah@gmail.com>
2024-05-28 16:02:40 +0330

    resaurant conflict fixed.

--- SofreDaar/SofreDaar/Models/Restaurant.cs
+++ SofreDaar/SofreDaar/Models/Restaurant.cs
@@ -3,10 +3,7 @@ namespace SofreDaar.Models;
 
 public class Restaurant:Base.Entity
 {
-    public string Name { get; set; }
     public string City { get; set; }
     public RestaurantReceptionType ReceptionType { get; set; }
     public string Address { get; set; }
-    
-    
 }
