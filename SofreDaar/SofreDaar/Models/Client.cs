9e48dc9ce412ef84adcdfc117becd6da697360ef
sseeyyeedd <sseeyyeeddaalliisah@gmail.com>
2024-07-02 09:04:52 +0330

    admin conflict with user fixed.

--- SofreDaar/SofreDaar/Models/Client.cs
+++ SofreDaar/SofreDaar/Models/Client.cs
@@ -11,6 +11,13 @@ public class Client:Base.User
     public ClinetSubscription Subscription { get; set; }
     public DateTime SubscriptionStart { get; set; }
     public string PaymentCode { get; set; }
+    [MaxLength(127)]
+    public string? SureName { get; set; }
+    public string? Email { get; set; }
+    [MaxLength(6)]
+    public string? VerificationCode { get; set; }
+    [MaxLength(11)]
+    public string? PhoneNumber { get; set; }
     public ICollection<Order> Orders { get; set; }
     public ICollection<Commnet> Commnets { get; set; }
     public ICollection<Report> Reports { get; set; }
@@ -20,6 +27,16 @@ public class Client:Base.User
         if (code == PaymentCode)
         {
             PaymentCode = "0";
+            return true;
+        }
+        return false;
+    }
+
+    public bool Activate(string code)
+    {
+        if (code == VerificationCode)
+        {
+            VerificationCode = "0";
             return true;
         }
         return false;
---
+++ SofreDaar/SofreDaar/ViewModels/RestaurantManagmentViewModel.cs
@@ -0,0 +1,76 @@
+﻿using SofreDaar.Helpers;
+using SofreDaar.Infrastructure;
+using SofreDaar.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SofreDaar.ViewModels
+{
+    public class RestaurantManagmentViewModel:BaseViewModel
+    {
+        public RestaurantManagmentViewModel(DatabaseContext DbContext, MainViewModel main) : base(DbContext, main)
+        {
+            AddRestaurantCommand=new RelayCommand(o =>
+            {
+                if (!Name.IsName())
+                {
+                    //show error
+                    return;
+                }
+                if (!Username.IsUsername()||DbContext.Clients.Any(x => x.Username==Username)||DbContext.Restaurants.Any(x => x.Username==Username)||DbContext.Admins.Any(x => x.Username==Username))
+                {
+                    //show error
+                    return;
+                }
+                var restaurant = new Restaurant()
+                {
+                    Id=Guid.NewGuid(),
+                    Name=Name,
+                    Username=Username,
+                    Password=Password,
+                    City=City
+
+                };
+                DbContext.Restaurants.Add(restaurant);
+                DbContext.SaveChanges();
+                Restaurants.Add(restaurant);
+            });
+            
+        }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged(); }
+        }
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; OnPropertyChanged(); }
+        }
+        private string _city;
+
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; OnPropertyChanged(); }
+        }
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; OnPropertyChanged(); }
+        }
+        public ICommand AddRestaurantCommand { get; set; }
+        public ObservableCollection<Restaurant> Restaurants { get; set; }
+    }
+}
