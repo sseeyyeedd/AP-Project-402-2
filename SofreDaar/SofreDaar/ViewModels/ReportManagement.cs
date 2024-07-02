5cebdfcbc35dab1ac0479abc64b5d47cede9084a
sseeyyeedd <sseeyyeeddaalliisah@gmail.com>
2024-07-02 17:22:21 +0330

    ReportManagment view added.

---
+++ SofreDaar/SofreDaar/ViewModels/ReportManagement.cs
@@ -0,0 +1,22 @@
+﻿using SofreDaar.Infrastructure;
+using SofreDaar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SofreDaar.ViewModels
+{
+    public class ReportManagement:BaseViewModel
+    {
+        public ReportManagement(DatabaseContext DbContext, MainViewModel main) : base(DbContext, main)
+        {
+            
+        }
+        public void UpdateReport(Report report)
+        {
+
+        }
+    }
+}
---
+++ SofreDaar/SofreDaar/Views/ReportManagement.xaml.cs
@@ -0,0 +1,62 @@
+﻿using SofreDaar.Models;
+using SofreDaar.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SofreDaar.Views
+{
+    /// <summary>
+    /// Interaction logic for ReportManagement.xaml
+    /// </summary>
+    public partial class ReportManagement : UserControl
+    {
+        public ReportManagement()
+        {
+            InitializeComponent();
+            DataContextChanged+=OnDataContextChanged;
+        }
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (DataContext is ProfileViewModel viewModel)
+            {
+                VM = viewModel;
+            }
+        }
+
+
+        public ProfileViewModel VM { get; set; }
+
+        private void DataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            var dataGrid = (DataGrid)sender;
+            if (e.EditAction == DataGridEditAction.Commit)
+            {
+                var editedItem = e.Row.Item as Report;
+                if (editedItem != null)
+                {
+                    var textBox = e.EditingElement as TextBox;
+                    if (textBox != null)
+                    {
+                        var newValue = textBox.Text;
+                        editedItem.Answer = newValue;
+                        editedItem.IsFollowedUp=true;
+
+                        VM.UpdateReport(editedItem);
+                    }
+                }
+            }
+        }
+    }
+}
